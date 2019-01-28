9b6b1ff4bae13d9cdd502c1a94863b1037cb55fd
CWD <cwd@cloudspokes.com>
2019-01-28 10:54:15 -0500

    added support for launching w/ local keys for kafka bus

--- config/default.js
+++ config/default.js
@@ -1,6 +1,12 @@
 /**
  * The default configuration file.
  */
+const fs = require('fs')
+
+function fileIfExists (path) {
+  console.log(path)
+  return fs.existsSync(path) ? path : null
+}
 
 module.exports = {
   DISABLE_LOGGING: process.env.DISABLE_LOGGING || false, // If true, logging will be disabled
@@ -11,19 +17,20 @@ module.exports = {
   KAFKA_CONSUMER_GROUP_ID: process.env.KAFKA_GROUP_ID || 'tc-submission-notification-processor-group',
   // below are used for secure Kafka connection, they are optional
   // for the local Kafka, they are not needed
-  KAFKA_CONSUMER_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT,
-  KAFKA_CONSUMER_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY,
-
+  KAFKA_CONSUMER_CLIENT_CERT: process.env.KAFKA_CONSUMER_CLIENT_CERT || fileIfExists(`${__dirname}/../kafkadev.cert`),
+  KAFKA_CONSUMER_CLIENT_CERT_KEY: process.env.KAFKA_CONSUMER_CLIENT_CERT_KEY || fileIfExists(
+    `${__dirname}/../kafkadev.key`),
   // Kafka producer config
   KAFKA_PRODUCER_URL: process.env.KAFKA_URL || 'localhost:9092',
   // below are used for secure Kafka connection, they are optional
   // for the local Kafka, they are not needed
-  KAFKA_PRODUCER_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT,
-  KAFKA_PRODUCER_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY,
+  KAFKA_PRODUCER_CLIENT_CERT: process.env.KAFKA_PRODUCER_CLIENT_CERT || fileIfExists(`${__dirname}/../kafkadev.cert`),
+  KAFKA_PRODUCER_CLIENT_CERT_KEY: process.env.KAFKA_PRODUCER_CLIENT_CERT_KEY || fileIfExists(
+    `${__dirname}/../kafkadev.key`),
 
   CREATE_NOTIFICATION_TOPIC: process.env.CREATE_NOTIFICATION_TOPIC || 'submission.notification.create',
   UPDATE_NOTIFICATION_TOPIC: process.env.UPDATE_NOTIFICATION_TOPIC || 'submission.notification.update',
-  PRODUCER_TOPIC: process.env.PRODUCER_TOPIC || 'test.email',
+  PRODUCER_TOPIC: process.env.PRODUCER_TOPIC || 'submission.notification.send',
 
   CHALLENGE_API_URL: process.env.CHALLENGE_API_URL || 'https://api.topcoder-dev.com/v4/challenges',
   SUBMISSION_API_URL: process.env.SUBMISSION_API_URL || 'https://api.topcoder-dev.com/v5/submissions',
